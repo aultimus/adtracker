169f03347a003622d3bd5ca2d8767475e1109959
aultimus <matthew.ault@cocoon.life>
2017-05-20 17:19:37 +0100

    timber: configure logging

--- adtracker/main.go
+++ adtracker/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/aultimus/adtracker"
+import (
+	"github.com/aultimus/adtracker"
+	"github.com/cocoonlife/timber"
+)
 
 func main() {
+	timber.AddLogger(timber.ConfigLogger{
+		LogWriter: new(timber.ConsoleWriter),
+		Level:     timber.DEBUG,
+		Formatter: timber.NewPatFormatter("[%D %T] [%L] %s %M"),
+	})
+
+	// TODO: make port configurable via flag
 	adtracker.Run(5000)
 }
